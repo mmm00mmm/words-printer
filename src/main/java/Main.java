6b2f38f955b6825a92a63b8783fe4634363e375a
mmm00mmm <54709088+mmm00mmm@users.noreply.github.com>
2019-09-23 18:24:58 +0900

    mainクラス

---
+++ src/main/java/Main.java
@@ -0,0 +1,58 @@
+import conversion.Capitalize;
+import conversion.Compress;
+import conversion.Reverse;
+import output.Horizontal;
+import output.Vertical;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<String> words = Arrays.asList(args[0].split(" "));
+
+        for (int i = 1; i < args.length; i++) {
+            switch (args[i]) {
+                case "--capitalize":
+                    Capitalize capitalize = new Capitalize(words);
+                    words = capitalize.各単語の先頭1文字を大文字にする();
+                    break;
+                case "--reverse":
+                    Reverse reverse = new Reverse(words);
+                    reverse.単語の順番を逆にする();
+                    break;
+                case "--compress":
+                    Compress compress = new Compress(words);
+                    words = compress.各単語の中の連続する文字を1文字にする(); //TODO impl
+                    break;
+                case "--horizontal":
+                    Horizontal horizontal = new Horizontal(words);
+                    horizontal.単語を半角スペースで区切って出力する();
+                    break;
+                case "--vertical":
+                    Vertical vertical = new Vertical(words);
+                    vertical.単語を改行で区切って出力する();
+                    break;
+            }
+        }
+
+//        List<String> list = Arrays.asList("this book is so funny".split(" "));
+//
+//        Capitalize capitalize = new Capitalize(list);
+//        System.out.println(capitalize.各単語の先頭1文字を大文字にする());
+//
+//        Reverse reverse = new Reverse(list);
+//        System.out.println(reverse.単語の順番を逆にする());
+//
+//        Compress compress = new Compress(list);
+//        System.out.println(compress.各単語の中の連続する文字を1文字にする());
+//
+//        Horizontal horizontal = new Horizontal(words);
+//        horizontal.単語を半角スペースで区切って出力する();
+//
+//        Vertical vertical = new Vertical(words);
+//        vertical.単語を改行で区切って出力する();
+    }
+}
